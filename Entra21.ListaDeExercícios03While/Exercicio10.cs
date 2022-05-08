956235d56ca550917691f649662125dae6b4b71c
Efraim Calebe Mertens <mertensefraim@gmail.com>
2022-05-08 10:55:41 -0300

    Exercício 10 funcionando

--- Entra21.ListaDeExercícios03While/Exercicio10.cs
+++ Entra21.ListaDeExercícios03While/Exercicio10.cs
@@ -10,7 +10,27 @@ namespace Entra21.ListaDeExercícios03While
     {
         public void Executar()
         {
+            Console.Write("Digite um número: ");
+            int numeroUsuario = Convert.ToInt32(Console.ReadLine());
+            int indice = 0, numerosAntecessores = numeroUsuario, numerosSucessores = numeroUsuario;
+            string numerosSucessoresResultado = "", numerosAntecessoresResultado = "";
+            while (indice < 100)
+            {
+                numerosAntecessores = numerosAntecessores - 1;
+                numerosSucessores = numerosSucessores + 1;
 
+                if(numerosAntecessores % 2 == 0)
+                {
+                    numerosAntecessoresResultado = numerosAntecessoresResultado + numerosAntecessores + "|";
+                }
+                if (numerosAntecessores % 2 != 0)
+                {
+                    numerosSucessoresResultado = numerosSucessoresResultado + numerosSucessores + "|";
+                }
+                indice = indice + 1;
+            }
+            Console.WriteLine("Antecessores pares: " + numerosAntecessoresResultado +
+                "\n\nSucessores ímpares: " + numerosSucessoresResultado);
         }
     }
 }
