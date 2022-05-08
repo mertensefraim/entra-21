d4affa71d2acb7da6244b7370cf7bf71a0b946e3
Efraim Calebe Mertens <mertensefraim@gmail.com>
2022-05-08 10:23:44 -0300

    Criado exercício 10

---
+++ Entra21.ListaDeExercícios03While/Exercicio10.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ListaDeExercícios03While
+{
+    internal class Exercicio10
+    {
+        public void Executar()
+        {
+
+        }
+    }
+}
--- Entra21.ListaDeExercícios03While/Program.cs
+++ Entra21.ListaDeExercícios03While/Program.cs
@@ -2,15 +2,16 @@
 using Entra21.ListaDeExercícios03While;
 
 Console.WriteLine(@"------------MENU------------
-1 - Exercício 01
-2 - Exercício 02
-3 - Exercício 03
-4 - Exercício 04    
-5 - Exercício 05
-6 - Exercício 06
-7 - Exercício 07
-8 - Exercício 08
-9 - Exercício 09");
+01 - Exercício 01
+02 - Exercício 02
+03 - Exercício 03
+04 - Exercício 04    
+05 - Exercício 05
+06 - Exercício 06
+07 - Exercício 07
+08 - Exercício 08
+09 - Exercício 09
+10 - Exercício 10");
 
 Console.Write("Digite a opção desejada: ");
 int opcaoDesejada = Convert.ToInt32(Console.ReadLine());
@@ -60,4 +61,9 @@ else if (opcaoDesejada == 9)
 {
     Exercicio09 exercicio09 = new Exercicio09();
     exercicio09.Executar();
+}
+else if (opcaoDesejada == 10)
+{
+    Exercicio10 exercicio10 = new Exercicio10();
+    exercicio10.Executar();
 }
