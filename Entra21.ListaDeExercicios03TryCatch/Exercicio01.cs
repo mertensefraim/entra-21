0e1b47659a0187abd803f37f7bea0281c990aff5
GuilhermeDMueller <guilhermemuellerdzesigaleski@gmail.com>
2022-05-13 21:55:01 -0300

    Criado exercício 1

---
+++ Entra21.ListaDeExercicios03TryCatch/Exercicio01.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ListaDeExercicios03TryCatch
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            var precoPeca = 0.0;
+            var nomePeca = "";
+            var texto = "";
+            for (var i = 0; i < 5; i++)
+            {
+                var numeroValido = false;
+                while (numeroValido == false)
+                {
+                    try
+                    {
+                        Console.Write("Digite preço da peça: ");
+                        precoPeca = Convert.ToDouble(Console.ReadLine());
+                        if (precoPeca < 0)
+                        {
+                            Console.WriteLine("Um valor negativo não é permitido");
+                        }
+                        else
+                        {
+                            numeroValido = true;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("O valor digitado não é permitido");
+                    }
+                }
+
+                numeroValido = false;
+                while (numeroValido == false)
+                {
+                    Console.Write("Digite o nome da peça: ");
+                    nomePeca = Console.ReadLine();
+                    if (nomePeca.Length > 15)
+                    {
+                        Console.WriteLine("O nome da peça não ser maior que quinze caracteres");
+                    }
+                    else if (nomePeca.Length < 2)
+                    {
+                        Console.WriteLine("O nome da peça não ser menor que dois caracteres");
+                    }
+                    else
+                    {
+                        numeroValido = true;
+                    }
+                }
+                texto = texto + nomePeca + "                  " + precoPeca + "\n";
+            }
+            Console.WriteLine(texto);
+        }
+    
+
---
+++ Entra21.ListaDeExercicios03TryCatch/Program.cs
@@ -0,0 +1,20 @@
+﻿using Entra21.ListaDeExercicios03TryCatch;
+
+Console.WriteLine(@"------------MENU------------
+01 - Exercício 01
+05 - Exercício 05
+08 - Exercício 08
+10 - Exercício 10
+12 - Exercício 12
+13 - Exercício 13
+");
+
+Console.Write("Digite a opção desejada: ");
+var opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+if (opcaoDesejada == 1)
+{
+    var exercicio01 = new Exercicio01();
+    exercicio01.Executar();
+}
