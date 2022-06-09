c1b36fb31db82f17f83106eb9e3600d1b49091b9
Efraim Mertens <mertensefraim@gmail.com>
2022-06-09 20:33:07 -0300

    Modificações controlador

--- Entra21.ExemplosListas/ProdutoControlador.cs
+++ Entra21.ExemplosListas/ProdutoControlador.cs
@@ -11,10 +11,75 @@ namespace Entra21.ExemplosListas
         private ProdutoServico produtoServico = new ProdutoServico();
         public void GerenciarMenu()
         {
-            Cadastrar();
-            ApresentarProdutos();
-            Cadastrar();
-            ApresentarProdutos();
+            int codigo = 0;
+            // Repete enquanto o código não for menu sair(6)
+            while (codigo != 6)
+            {
+                Console.Clear();
+                // Apresenta o menu e solicita o código
+                codigo = ApresentarSolicitarMenu();
+
+                Console.Clear();
+
+                if (codigo == 1)
+                {
+                    // Menu escolhido para listar produtos
+                    ApresentarProdutos();
+                }
+                else if (codigo == 2)
+                {
+                    // Menu escolhido para cadastar produto
+                    Cadastrar();
+                }
+                else if (codigo == 3)
+                {
+                    // Menu escolhido para editar produto
+                    // Editar();
+                }
+                else if (codigo == 4)
+                {
+                    // Apagar();
+                }
+                else if (codigo == 5)
+                {
+                    // ApresentarProduto();
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        private int ApresentarSolicitarMenu()
+        {
+            Console.WriteLine(@"MENU: 
+1 - Listar todos
+2 - Cadastrar
+3 - Editar
+4 - Apagar
+5 - Apresentar produto desejado
+6 - Sair");
+
+            int codigo = SolicitarCodigo();
+
+            return codigo;
+        }
+
+        private int SolicitarCodigo()
+        {
+            int codigo = 0;
+            // Continua solicitando o código até que um seja um código entre 1 e 6
+            while (codigo < 1 || codigo > 6)
+            {
+                try
+                {
+                    Console.Write("Digite o código: ");
+                    codigo = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Digite um menu válido");
+                }
+            }
+            return codigo;
         }
         private void Cadastrar()
         {
@@ -27,6 +92,10 @@ namespace Entra21.ExemplosListas
             Console.Write("Preço unitário: ");
             var precoUnitario = Convert.ToDouble(Console.ReadLine());
 
+            Console.WriteLine(@"Localizações disponíveis:
+- Armazem
+- Area Venda
+- Loja");
             Console.Write("Localização: ");
             var localizacao = Console.ReadLine();
 
@@ -52,6 +121,13 @@ namespace Entra21.ExemplosListas
         private void ApresentarProdutos()
         {
             var produtos = produtoServico.ObterTodos();
+
+            if (produtos.Count == 0)
+            {
+                Console.WriteLine("Nenhum produto cadastrado :(");
+                // Para a execução deste método
+                return;
+            }
 
             for (var i = 0; i < produtos.Count; i++)
             {
