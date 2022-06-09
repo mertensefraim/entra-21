07f7fe32ce1b337506ee382570eb2232c2668ac2
Efraim Mertens <mertensefraim@gmail.com>
2022-06-09 20:00:33 -0300

    Serviço do exemplo de listas funcionando

--- Entra21.ExemplosListas/ProdutoServico.cs
+++ Entra21.ExemplosListas/ProdutoServico.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entra21.ExemplosListas
+﻿namespace Entra21.ExemplosListas
 {
     internal class ProdutoServico
     {
         private List<Produto> produtos = new List<Produto>();
+        // Armazenar o código do próximo produto
+        private int codigoAtual = 1;
 
         public void Adicionar(string nome, double precoUnitario, ProdutoLocalizacao localizacao, int quantidade)
         {
@@ -21,18 +17,61 @@ namespace Entra21.ExemplosListas
             produto.Localizacao = localizacao;
             produto.Quantidade = quantidade;
 
+            // Atribuir o código novo para o objeto do produto
+            produto.Codigo = codigoAtual;
+
+            // Adicionar 1 no código atual para quando criar um novo produto o produto conter outro código
+            codigoAtual++;
+
             // Adicionar o produto instaciando na lista de produtos
             produtos.Add(produto);
         }
 
-        public void Editar()
+        public bool Editar(int codigoParaAlterar, string nome, double precoUnitario, ProdutoLocalizacao localizacao, int quantidade)
         {
+            // Obtém o produto desejado da lista do produto
+            Produto produtoParaAlterar = ObterPorCodigo(codigoParaAlterar);
 
+            // Verifica se não foi possível encontrar o produto
+            if (produtoParaAlterar == null)
+            {
+                // Retorna falso pq não existe produto com o código dos parâmetros.
+                return false;
+            }
+
+            // Atualiza as propriedades (campos) do produto desejado
+            produtoParaAlterar.Nome = nome;
+            produtoParaAlterar.Localizacao = localizacao;
+            produtoParaAlterar.PrecoUnitario = precoUnitario;
+            produtoParaAlterar.Quantidade = quantidade;
+
+            // Retorna verdadeiro pq foi possível alterar o produto
+            return true;
         }
 
-        public void Apagar()
+        public bool Apagar(int codigo)
         {
+            for (int i = 0; i < produtos.Count; i++)
+            {
+                // Resgatando de uma lista de tipos primitivos
+                // int numero = numeros[i]
 
+                // Resgatar de uma lista de objetos
+                Produto produto = produtos[i];
+                if (produto.Codigo == codigo)
+                {
+                    // Posso remover com o índice do objeto desejado
+                    produtos.RemoveAt(i);
+
+                    // Posso remover com o objeto desejado
+                    // produtos.Remove(produto);
+
+                    // Retorna verdadeiro pq o produto com o código desejado foi removido da lista
+                    return true;
+                }
+            }
+            // Retorna falso pq o produto com o código desejado foi removido da lista
+            return false;
         }
 
         public List<Produto> ObterTodos()
@@ -40,8 +79,23 @@ namespace Entra21.ExemplosListas
             return produtos;
         }
 
-        public Produto ObterPorCodigo()
+        public Produto ObterPorCodigo(int codigo)
         {
+            // Percorrer todos os elementos para encontrar o produto filtrando por código
+            for (int i = 0; i < produtos.Count; i++)
+            {
+                // Obter o produto da lista de produtos em determinado indice
+                var produtoAtual = produtos[i];
+
+                // Verifica se o produto atual da lista percorrida é desejado
+                if (produtoAtual.Codigo == codigo)
+                {
+                    // Retorna o produto encontrado
+                    return produtoAtual;
+                }
+            }
+
+            // Retorna null, caso não encontre um produto com código passado com parâmetro
             return null;
         }
     }
