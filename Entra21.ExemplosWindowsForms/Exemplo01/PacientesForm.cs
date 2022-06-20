374dfb4bd1ed53642f1c1a8e23a699594f6bead6
Efraim Mertens <mertensefraim@gmail.com>
2022-06-20 20:52:16 -0300

    Exemplo Windows Forms finalizado

--- Entra21.ExemplosWindowsForms/Exemplo01/PacientesForm.cs
+++ Entra21.ExemplosWindowsForms/Exemplo01/PacientesForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,31 +13,43 @@ namespace Entra21.ExemplosWindowsForms.Exemplo01
 {
     public partial class PacientesForm : Form
     {
-        private int codigo = 1;
+        private List<Paciente> pacientes;
+
+        private int codigo = 0;
         private int indiceLinhaSelecionada = -1;
         private int codigoSelecionado = -1;
 
         public PacientesForm()
         {
             InitializeComponent();
+
+            // Cria uma lista de objetos para armazenar os pacientes
+            pacientes = new List<Paciente>();
+
+            // Ler do arquivo JSON os pacientes cadastrados anteriormente                
+            LerArquivoApresentandoPacientes();
         }
 
         private void buttonSalvar_Click(object sender, EventArgs e)
         {
+            // Obter as informações dos campos
             var nome = textBoxNome.Text.Trim();
             var altura = Convert.ToDouble(textBoxAltura.Text.Trim());
             var peso = Convert.ToDouble(textBoxPeso.Text.Trim());
 
-            var imc = peso / Math.Pow(altura, 2);
+            // Calcular IMC
+            var imc = CalcularImc(peso, altura);
 
             // Verifica se está em modo de adição
             if (indiceLinhaSelecionada == -1)
-            {
+            { 
                 dataGridViewInformacoes.Rows.Add(new object[]
                 {
-                codigo++, nome, altura, peso, imc
+                ++codigo, nome, altura, peso, imc
                 });
 
+                AdicionarPacienteSalvandoNoArquivo(codigo, nome, peso, altura);
+              
                 return;
             }
 
@@ -44,7 +57,19 @@ namespace Entra21.ExemplosWindowsForms.Exemplo01
             dataGridViewInformacoes.Rows[indiceLinhaSelecionada].Cells[2].Value = altura.ToString();
             dataGridViewInformacoes.Rows[indiceLinhaSelecionada].Cells[3].Value = peso.ToString();
             dataGridViewInformacoes.Rows[indiceLinhaSelecionada].Cells[4].Value = imc.ToString();
-            
+
+            EditarDados(nome, peso, altura);
+        }
+
+        private void LimparCampos()
+        {
+            textBoxPeso.Text = "";
+            textBoxNome.Text = "";
+            textBoxAltura.Text = "";
+
+            indiceLinhaSelecionada = -1;
+
+            dataGridViewInformacoes.ClearSelection();
         }
 
         private void buttonApagar_Click(object sender, EventArgs e)
@@ -66,6 +91,12 @@ namespace Entra21.ExemplosWindowsForms.Exemplo01
             {
                 // Remove a linha utilizando o indice do DataGriedView
                 dataGridViewInformacoes.Rows.RemoveAt(indiceLinhaSelecionada);
+
+                // Remove o paciente da lista de pacientes
+                pacientes.RemoveAt(indiceLinhaSelecionada);
+
+                // Atualiza o arquivo com lista de pacientes sem o paciente removido
+                SalvarEmArquivo();
             }
         }
 
@@ -91,6 +122,87 @@ namespace Entra21.ExemplosWindowsForms.Exemplo01
             textBoxAltura.Text = altura.ToString();
             textBoxPeso.Text = peso.ToString();
 
+        }
+
+        private void EditarDados(string nome, double peso, double altura)
+        {
+            pacientes[indiceLinhaSelecionada].Nome = nome;
+            pacientes[indiceLinhaSelecionada].Peso = peso;
+            pacientes[indiceLinhaSelecionada].Altura = altura;
+
+            SalvarEmArquivo();
+
+            LimparCampos();
+        }
+
+        private void AdicionarPacienteSalvandoNoArquivo(int codigo, string nome, double peso, double altura)
+        {
+            var paciente = new Paciente
+            {
+                Codigo = codigo,
+                Nome = nome,
+                Altura = altura,
+                Peso = peso,
+            };
+            // var paciente = new Paciente();
+            // paciente.Codigo = codigo;
+            // paciente.Nome = nome;
+
+            pacientes.Add(paciente);
+
+            SalvarEmArquivo();
+            LimparCampos();
+        }
+
+        private void SalvarEmArquivo()
+        {
+            // Converter uma lista de objetos em uma string contendo o JSON
+            var pacientesEmJson = JsonConvert.SerializeObject(pacientes);
+            var caminho = "pacientes.json";
+            // Salvar a string contendo o Json em um arquivo no formato JSON
+            File.WriteAllText(caminho, pacientesEmJson);
+        }
+
+        private void LerArquivoApresentandoPacientes()
+        {
+            // Validar se arquivo não existe, consequentemente não é necessário percorrer uma lista que não existe
+
+            if (File.Exists("pacientes.json") == false)
+                return;
+
+            // Ler arquivo json e armazenar os pacientes na lista de paientes
+            var conteudoArquivo = File.ReadAllText("pacientes.json");
+            pacientes = JsonConvert.DeserializeObject<List<Paciente>>(conteudoArquivo);
+
+            // Cria uma variável para armazenar o código do último paciente para quando
+            // cadastrar um novo armazenar com código correto
+            var maiorCodigo = int.MinValue;
+
+            for (int i = 0; i < pacientes.Count; i++)
+            {
+                // Obtém o paciente que está sendo percorrido
+                var paciente = pacientes[i];
+
+                // Adicionar o paciente que estava no arquivo json no DataGridView
+                dataGridViewInformacoes.Rows.Add(new object[]
+                {
+                paciente.Codigo, paciente.Nome,
+                paciente.Altura, paciente.Peso,
+                CalcularImc(paciente.Peso, paciente.Altura)
+                });
+                // Rotina para descobrir o paciente com o maior código
+                if (paciente.Codigo > maiorCodigo)
+                    maiorCodigo = paciente.Codigo;
+            }
+            // Validar se conseguiu encontar algum código, caso contrário
+            // n deve atualizar o codigo do novo paciente
+            if (maiorCodigo != int.MinValue)
+                codigo = maiorCodigo;
+        }
+
+        private double CalcularImc(double peso, double altura)
+        {
+            return peso / Math.Pow(altura, 2);
         }
     }
 }
