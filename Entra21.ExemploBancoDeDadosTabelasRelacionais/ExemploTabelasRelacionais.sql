c0967247fd4b380b7c2cb883b7d45df5ba1d2d76
Efraim Mertens <mertensefraim@gmail.com>
2022-07-13 22:13:09 -0300

    Exemplos de banco de dados

--- Entra21.ExemploBancoDeDadosTabelasRelacionais/ExemploTabelasRelacionais.sql
+++ Entra21.ExemploBancoDeDadosTabelasRelacionais/ExemploTabelasRelacionais.sql
@@ -124,3 +124,108 @@ SELECT
 	INNER JOIN clientes AS c ON(p.id_cliente = c.id);
 
 UPDATE pedidos SET status = 1 WHERE id = 2;
+
+-- Adicionar as peças ao pedido
+INSERT INTO pedidos_pecas(id_pedido, id_peca, quantidade) VALUES
+(1, 2, 2), -- 2 SSDs M2 para o pedido 1
+(1, 4, 1), -- 1 GTX 1060 para o pedido 1
+(1, 6, 1); -- 1 módulo 16gb Ram DDR5
+
+-- Alterar o cliente do pedido 1 para o cliente Cry
+UPDATE pedidos SET id_cliente = 2 WHERE id = 1;
+
+-- Consultar apresentando nome cliente, nome peça,
+-- quantidade, valor_unitário, total das peças
+SELECT 
+	pd.id AS 'Código pedido',
+	c.nome AS 'Nome',
+	p.nome AS 'Peça',
+	pp.quantidade AS 'Quantiadde',
+	CONCAT('R$ ', p.preco_unitario) AS 'Valor unitário',
+	CONCAT('R$ ', p.preco_unitario * pp.quantidade) AS 'Total das peças'
+	FROM pedidos_pecas AS pp
+	INNER JOIN pecas AS p ON(pp.id_peca = p.id)
+	INNER JOIN pedidos AS pd ON(pp.id_pedido = pd.id)
+	INNER JOIN clientes AS c ON(pd.id_cliente = c.id);
+
+INSERT INTO pedidos(id_cliente, data_criacao, status) VALUES
+(1, GETDATE(), 0); -- GETDATE() é o mesmo que o DateTime.Now
+
+INSERT INTO pedidos_pecas(id_pedido, id_peca, quantidade) VALUES
+(3, 2, 2), -- id_pedido = 3, id_peca = 2 (SSD 240M2), quantidade = 2
+(3, 3, 2), -- id_pedido = 3, id_peca = 3 (RTX3090 TI), quantidade = 2
+(3, 5, 4); -- id_pedido = 3, id_peca = 5 (16Gb RAM DDR53), quantidade = 4 Quad Channel ( :
+
+SELECT 
+	p.id AS 'Código pedido',
+	p.status AS 'Status pedido',
+	c.nome AS 'Cliente',
+	CONCAT(
+		e.estado, ' ',
+		e.cidade, ' ',
+		e.bairro, ' ',
+		e.logradouro, ' ',
+		e.numero) AS 'Endereço completo'
+	FROM pedidos AS p
+	INNER JOIN clientes AS c ON(p.id_cliente = c.id)
+	INNER JOIN enderecos AS e ON(c.id = e.id_cliente)
+	WHERE p.id_cliente = (SELECT id FROM clientes WHERE cpf = '070.355.489-73');
+
+-- Efetivar a compra do pedido do Cláudio
+UPDATE pedidos
+	SET 
+		status = 2,
+		data_efetivacao_compra = '2022-07-12 17:30:00'
+	WHERE
+		id = 3;
+
+SELECT * FROM pedidos;
+
+-- COnsultar peças do pedido do CLaudio
+
+SELECT 
+	p.id AS 'Código pedido',
+	p.status AS 'Status pedido',
+	c.nome AS 'Cliente',
+	pec.nome AS 'Peça'
+	FROM pedidos AS p
+	INNER JOIN clientes AS c ON(p.id_cliente = c.id)
+	INNER JOIN enderecos AS e ON(c.id = e.id_cliente)
+	INNER JOIN pedidos_pecas AS pp ON(p.id = pp.id_pedido)
+	INNER JOIN pecas AS pec ON(pp.id_peca = pec.id)
+	WHERE p.id_cliente = (SELECT id FROM clientes WHERE cpf = '070.355.489-73');
+
+SELECT * FROM pedidos;
+
+-- pegar nome cliente, nome peça, rua
+
+SELECT 
+	c.nome AS 'Cliente',
+	pec.nome AS 'peças',
+	e.logradouro
+	FROM pedidos AS p
+	INNER JOIN clientes AS c ON(p.id_cliente = c.id)
+	INNER JOIN pedidos_pecas AS pp ON(p.id = pp.id_peca)
+	INNER JOIN pecas AS pec ON(pp.id_peca = pec.id)
+	INNER JOIN enderecos AS e ON(c.id = e.id_cliente)
+	
+-- Nome cliente, nome peça pelo número do endereco
+SELECT
+	c.nome,
+	pec.nome
+	FROM pedidos AS p
+	INNER JOIN clientes AS c ON(p.id_cliente = c.id)
+	INNER JOIN pedidos_pecas AS pp ON(p.id = pp.id_pedido)
+	INNER JOIN pecas AS pec ON(pp.id_peca = pec.id)
+	WHERE id_cliente = (SELECT id FROM enderecos WHERE numero = '777');
+
+SELECT 
+	pec.preco_unitario,
+	pec.nome,
+	c.nome,
+	e.cidade
+	FROM pecas AS pec
+	INNER JOIN pedidos_pecas AS pp ON(pp.id_peca = pec.id)
+	INNER JOIN pedidos AS p ON(pp.id_pedido = p.id)
+	INNER JOIN clientes AS c ON(p.id_cliente = c.id)
+	INNER JOIN enderecos AS e ON(c.id = e.id_cliente)
